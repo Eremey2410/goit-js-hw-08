90a0f3a05bedc37db694fbe4aa516b0387559723
Eremey2410 <2410esv@gmail.com>
2022-07-29 22:38:18 +0300

    add 1,2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,34 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
+const galleryEl = document.querySelector('.gallery');
+const galleryMarcup = createGalleryItemsMarkup(galleryItems);
+
+galleryEl.insertAdjacentHTML('beforeend', galleryMarcup);
+
+// console.log(createGalleryItemsMarkup(galleryItems));
+function createGalleryItemsMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+    <div class="gallery__item">
+       <a class="gallery__item" href="${original}">
+            <img 
+                class="gallery__image" 
+                src="${preview}" 
+                alt="${description}" 
+            />
+        </a>
+    </div>
+        `;
+    })
+    .join('');
+}
+var lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
 
 console.log(galleryItems);
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,60 @@
+import Player from '@vimeo/player';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+let arr = [];
+
+player.on('play', function (data) {
+  //   console.log(date.seconds);
+  localStorage.setItem('videoplayer-current-time', data.seconds);
+  const ger = localStorage.getItem('videoplayer-current-time');
+  arr.push(ger);
+  console.log(Number(arr));
+});
+player
+  .setCurrentTime(Number(arr.length - 1))
+  .then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+
+      default:
+        // some other error occurred
+        break;
+    }
+  });
+
+// console.log(player);
+
+// player.on('timeupdate', onPlay);
+// let timeArray = [];
+// function onPlay(data) {
+//   console.log(data);
+
+//   localStorage.setItem('videoplayer-current-time', JSON.stringify(data));
+//   const curTime = localStorage.getItem('videoplayer-current-time');
+//   const parsedCurTime = JSON.parse(curTime);
+//   //   console.log(parsedCurTime.seconds);
+//   timeArray.push(parsedCurTime.seconds);
+// }
+// console.log(timeArray);
+// player
+//   .setCurrentTime(timeArray.length - 1)
+//   .then(function (seconds) {
+//     // seconds = the actual time that the player seeked to
+//   })
+//   .catch(function (error) {
+//     switch (error.name) {
+//       case 'RangeError':
+//         // the time was less than 0 or greater than the video’s duration
+//         break;
+
+//       default:
+//         // some other error occurred
+//         break;
+//     }
+//   });
