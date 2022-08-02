50290004180c4c757739d9ee5840a947d91e8abf
Eremey2410 <2410esv@gmail.com>
2022-08-02 20:36:19 +0300

    done 8-2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,11 +1,13 @@
 import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 const VIDEOPLAYER_CURRENT_TIME = 'videoplayer-current-time';
 const savedTime = localStorage.getItem(VIDEOPLAYER_CURRENT_TIME);
-player.on('timeupdate', onPlay);
-console.log(player);
+const trottledOnPlay = throttle(onPlay, 1000);
+
+player.on('timeupdate', trottledOnPlay);
 
 function onPlay(data) {
   localStorage.setItem(VIDEOPLAYER_CURRENT_TIME, data.seconds);
@@ -18,22 +20,3 @@ if (savedTime) {
 }
 
 player.setCurrentTime(parsedSeconds);
-
-// import throttle from 'lodash.throttle';
-// import Player from '@vimeo/player';
-
-// const VIDEOPLAYER_CURRENT_TIME = 'videoplayer-current-time';
-// const iframe = document.querySelector('#vimeo-player');
-// const player = new Player(iframe);
-// const getSavedTime = localStorage.getItem(VIDEOPLAYER_CURRENT_TIME);
-
-// if (getSavedTime) {
-//   player.setCurrentTime(getSavedTime);
-// }
-
-// player.on('timeupdate', throttle(onTimeUpdate, 1000));
-
-// function onTimeUpdate(time) {
-//   let savedPausedPlayerTime = time.seconds;
-//   localStorage.setItem(VIDEOPLAYER_CURRENT_TIME, savedPausedPlayerTime);
-// }
