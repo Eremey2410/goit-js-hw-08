b65c2f0339661793a9eeb39fe1209f32cba9326e
Eremey2410 <2410esv@gmail.com>
2022-08-04 22:11:05 +0300

    .

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,23 +1,20 @@
-import Player from '@vimeo/player';
+//
+
 import throttle from 'lodash.throttle';
+import Player from '@vimeo/player';
 
-const iframe = document.querySelector('iframe');
-const player = new Player(iframe);
 const VIDEOPLAYER_CURRENT_TIME = 'videoplayer-current-time';
-const trottledOnPlay = throttle(onPlay, 1000);
-getSavedTime();
-player.on('timeupdate', trottledOnPlay);
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+const getSavedTime = localStorage.getItem(VIDEOPLAYER_CURRENT_TIME);
 
-function onPlay(data) {
-  localStorage.setItem(VIDEOPLAYER_CURRENT_TIME, data.seconds);
+if (getSavedTime) {
+  player.setCurrentTime(getSavedTime);
 }
-function getSavedTime() {
-  const savedTime = localStorage.getItem(VIDEOPLAYER_CURRENT_TIME);
 
-  if (savedTime) {
-    parsedSeconds = JSON.parse(savedTime);
-    player.setCurrentTime(parsedSeconds);
-  } else {
-    parsedSeconds = 0;
-  }
+player.on('timeupdate', throttle(onTimeUpdate, 1000));
+
+function onTimeUpdate(time) {
+  let savedPausedPlayerTime = time.seconds;
+  localStorage.setItem(VIDEOPLAYER_CURRENT_TIME, savedPausedPlayerTime);
 }
