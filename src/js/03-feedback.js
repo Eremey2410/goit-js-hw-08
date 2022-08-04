de187ffef35772ec9eba3426ea5f459063ce271f
Eremey2410 <2410esv@gmail.com>
2022-08-04 19:28:01 +0300

    done 8-3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,17 +1,34 @@
+import throttle from 'lodash.throttle';
+
 const form = document.querySelector('.feedback-form');
-const LOCALSTORAGE_KEY = 'feedback-form-state';
-console.log(form);
-form.addEventListener('input', onInput);
-function onInput(event) {
-  event.preventDefault();
-  const event = {
-    email: email,
-    message: message,
-  };
-  localStorage.setItem(LOCALSTORAGE_KEY, event.target);
-  console.log(event);
-  test();
+
+const FORM_LOCALSTORAGE_KEY = 'feedback-form-state';
+const formData = {};
+
+form.addEventListener('submit', onFormSubmit);
+form.addEventListener('input', throttle(onTextareaInput, 500));
+
+populatedForm();
+
+function onFormSubmit(e) {
+  e.preventDefault();
+
+  e.currentTarget.reset();
+  localStorage.removeItem(FORM_LOCALSTORAGE_KEY);
+  console.log('Submit:', formData);
 }
-function test() {
-  form.textContent = localStorage.getItem(LOCALSTORAGE_KEY);
+function onTextareaInput(e) {
+  formData[e.target.name] = e.target.value;
+
+  localStorage.setItem(FORM_LOCALSTORAGE_KEY, JSON.stringify(formData));
+}
+function populatedForm() {
+  const savedMessage = localStorage.getItem(FORM_LOCALSTORAGE_KEY);
+  const parsedSavedMessage = JSON.parse(savedMessage);
+  if (parsedSavedMessage) {
+    for (const key in parsedSavedMessage) {
+      document.querySelector(`[name = ${key}]`).value = parsedSavedMessage[key];
+    }
+    console.log('Значение при перезагрузке', parsedSavedMessage);
+  }
 }
