b3e72013fd582e71b6b38bdeca68f6011ceb3bff
Eremey2410 <2410esv@gmail.com>
2022-08-04 22:01:49 +0300

    .

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,18 +4,20 @@ import throttle from 'lodash.throttle';
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 const VIDEOPLAYER_CURRENT_TIME = 'videoplayer-current-time';
-const savedTime = localStorage.getItem(VIDEOPLAYER_CURRENT_TIME);
 const trottledOnPlay = throttle(onPlay, 1000);
-
+getSavedTime();
 player.on('timeupdate', trottledOnPlay);
 
 function onPlay(data) {
   localStorage.setItem(VIDEOPLAYER_CURRENT_TIME, data.seconds);
 }
+function getSavedTime() {
+  const savedTime = localStorage.getItem(VIDEOPLAYER_CURRENT_TIME);
 
-if (savedTime) {
-  parsedSeconds = JSON.parse(savedTime);
-  player.setCurrentTime(parsedSeconds);
-} else {
-  parsedSeconds = 0;
+  if (savedTime) {
+    parsedSeconds = JSON.parse(savedTime);
+    player.setCurrentTime(parsedSeconds);
+  } else {
+    parsedSeconds = 0;
+  }
 }
